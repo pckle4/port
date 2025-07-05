55cb88fe97b6af57175fe1e6a8a40ef42546f0d7
pckle4 <theanshah@gmail.com>
2025-07-05 20:21:43 +0530

    Add files via upload

---
+++ lib/fonts.ts
@@ -0,0 +1,8 @@
+import { Kalam } from "next/font/google"
+
+export const kalam = Kalam({
+  subsets: ["latin"],
+  weight: ["300", "400", "700"],
+  variable: "--font-kalam",
+  display: "swap",
+})
